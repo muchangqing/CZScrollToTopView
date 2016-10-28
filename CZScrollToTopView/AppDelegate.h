3f2e95c2f1cb57e2ed5fd68df1110bd0b1f661f0
大好时光 <mcq@github.com>
2016-10-28 18:21:57 +0800

    Initial Commit

---
+++ CZScrollToTopView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CZScrollToTopView
+//
+//  Created by LingVR on 16/10/28.
+//  Copyright © 2016年 muchangqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CZScrollToTopView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CZScrollToTopView
+//
+//  Created by LingVR on 16/10/28.
+//  Copyright © 2016年 muchangqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
